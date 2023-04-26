174eac97cb73f9ff46267e44625d0592d14d388c
Soran <soranit2000@gmail.com>
2023-04-26 15:50:07 +0430

    Fixed Footer bug using flex box

--- src/Components/Layouts/PageLayout/index.js
+++ src/Components/Layouts/PageLayout/index.js
@@ -1,12 +1,16 @@
 import Footer from "../Footer";
 import {Navbar} from "../Navbar";
+import "./style.css"
 
 const PageLayout = ({children, footer = true}) => {
-    return <>
-        <Navbar/>
-        {children}
-        {footer === true && <Footer/>}
-    </>
+    return(
+        <div className="layOut">
+            <Navbar/>
+            {children}
+            {footer === true && <Footer/>}
+        </div>
+    )
+
 
 }
 export default PageLayout
